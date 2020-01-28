d920da19f05dc70865af7524105983568c2b1313
wenyou.zheng@magnumwm.com <wenyou.zheng@magnumwm.com>
2020-01-28 20:57:57 +0800

    35

--- code/recursive.py
+++ code/recursive.py
@@ -1,8 +1,25 @@
 
 
 class Recursive(object):
-    pass
+
+    def searchInsert(self, nums: list, target: int) -> int:
+        if not nums:
+            return 0
+        length = len(nums)
+        if length == 1:
+            if target <= nums[0]:
+                return 0
+            else:
+                return 1
+        half_length = length // 2
+        if target >= nums[half_length]:
+            return half_length + self.searchInsert(nums[half_length:], target)
+        else:
+            return self.searchInsert(nums[:half_length], target)
 
 
 if __name__ == '__main__':
-    pass
+    a = [1, 3, 5, 6]
+    b = 0
+    rec = Recursive()
+    print(rec.searchInsert(a, b))
