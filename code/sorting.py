ab4035901ff4ad44adbb95f9d95ade66f8577214
wenyou.zheng@magnumwm.com <wenyou.zheng@magnumwm.com>
2020-10-20 08:10:12 +0800

    update shell sort

--- code/sorting.py
+++ code/sorting.py
@@ -23,9 +23,21 @@ class Sorting(object):
                     self.source_list = self.source_list[:j] + [tmp] + self.source_list[j:i] + self.source_list[(i + 1):]
                     break
 
+    def shell_sort(self):
+        increment = len(self.source_list) // 2
+        for i in range(len(self.source_list)):
+            tmp = i
+            while tmp < len(self.source_list):
+                tmp += increment
+                for j in range(i, tmp, increment):
+                    if tmp < self.source_list[j]:
+                        self.source_list = self.source_list[:j] + [tmp] + self.source_list[j:i] + self.source_list[(i + 1):]
+                        break
+
 
 if __name__ == '__main__':
     sorting = Sorting()
-    sorting.generate_random_list(num=5000)
-    sorting.insert_sort()
+    sorting.generate_random_list(num=500)
+     #sorting.insert_sort()
+    sorting.shell_sort()
     # print(sorting.source_list)
